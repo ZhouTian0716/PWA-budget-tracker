437df60f7c471aaa549e3ae20f634a8d736c99b5
ZhouTian0716 <egglord0716@gmail.com>
2021-07-12 00:29:00 +1000

    turning to pwa

---
+++ public/service-worker.js
@@ -0,0 +1,86 @@
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/assets/css/style.css",
+    "/assets/js/db.js",
+    "/assets/js/index.js",
+    "/manifest.webmanifest",
+    "/favicon.ico",
+    "/assets/images/icons/icon-72x72.png",
+    "/assets/images/icons/icon-96x96.png",
+    "/assets/images/icons/icon-128x128.png",
+    "/assets/images/icons/icon-144x144.png",
+    "/assets/images/icons/icon-152x152.png",
+    "/assets/images/icons/icon-192x192.png",
+    "/assets/images/icons/icon-384x384.png",
+    "/assets/images/icons/icon-512x512.png",
+  ];
+  
+  // need to update version if the above array get updated
+  const CACHE_NAME = "static-cache-v1";
+  const DATA_CACHE_NAME = "data-cache-v1";
+  
+  // install
+  self.addEventListener("install", function(event) {
+    event.waitUntil(
+      caches.open(CACHE_NAME).then(cache => {
+        console.log("Your files were pre-cached successfully!");
+        return cache.addAll(FILES_TO_CACHE);
+      })
+    );
+    // self is the service worker instant
+    self.skipWaiting();
+  });
+  
+  self.addEventListener("activate", function(event) {
+    event.waitUntil(
+      caches.keys().then(keyList => {
+        return Promise.all(
+          keyList.map(key => {
+            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+              console.log("Removing old cache data", key);
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+    );
+  
+    self.clients.claim();
+  });
+  
+  // fetch
+  self.addEventListener("fetch", function(event) {
+    // cache successful requests to the API
+    if (event.request.url.includes("/api/")) {
+      event.respondWith(
+        caches.open(DATA_CACHE_NAME).then(cache => {
+          return fetch(event.request)
+            .then(response => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(event.request.url, response.clone());
+              }
+  
+              return response;
+            })
+            .catch(err => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(event.request);
+            });
+        }).catch(err => console.log(err))
+      );
+  
+      return;
+    }
+  
+    // if the request is not for the API, serve static assets using "offline-first" approach.
+    // see https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook#cache-falling-back-to-network
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
+  
